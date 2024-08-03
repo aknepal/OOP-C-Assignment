1a1c9da08f1c35b52a0ccf5829db12dd4c8b5ea0
kaushalrijal <kaushalrijal091@gmail.com>
2024-08-03 11:56:36 +0545

    create basic class structure for problem 4

--- problem4.cpp
+++ problem4.cpp
@@ -5,3 +5,40 @@ Implement polymorphic behavior where the base class pointer can point to objects
 derived classes, and call their respective member functions to deposit and withdraw
 money, applying specific rules for each account type.
 */
+
+#include<iostream>
+
+using namespace std;
+
+class Account{
+    int accountNumber;
+    float balance;
+
+    public:
+        virtual void depositMoney() = 0;
+        virtual void withdrawMoney() = 0;
+};
+
+class SavingsAccount : public Account{
+    public:
+        void depositMoney() override{
+            cout << "deposit function called from within savings account";
+        }
+        void withdrawMoney() override{
+            cout << "withdraw function called from within savings account";
+        }
+};
+
+class CheckingAccount : public Account{
+    public:
+        void depositMoney() override{
+            cout << "deposity function called from within checking account";
+        }
+        void withdrawMoney() override{
+            cout << "withdraw function called from within checking account";
+        }
+};
+
+int main(){
+    
+}
